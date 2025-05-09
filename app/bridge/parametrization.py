853baa27feaf94267d785d545954b07d2cc78aef
Quincy de Klerk <q.klerk@amsterdam.nl>
2025-05-09 16:37:39 +0200

    changed zone thickness control for zone 2 in the parameter

--- app/bridge/parametrization.py
+++ app/bridge/parametrization.py
@@ -47,7 +47,8 @@ Elk item in de lijst hieronder representeert een dwarsdoorsnede.
 - Elk **volgend item** definieert de geometrie van de *volgende* dwarsdoorsnede (D2, D3, etc.).
 - Het veld '**Afstand tot vorige snede**' (`l`) geeft de lengte van het brugsegment *tussen* de voorgaande en de huidige snede.
   Dit veld is niet zichtbaar voor de eerste snede.
-- De overige dimensievelden (zoals `bz1`, `bz2`, `dz`) beschrijven de eigenschappen van de *huidige* dwarsdoorsnede.
+- De overige dimensievelden (zoals `bz1`, `bz2`, `dz` voor de dikte van zone 1 en 3, en `dz_2` voor de dikte van zone 2)
+  beschrijven de eigenschappen van de *huidige* dwarsdoorsnede.
 Standaard zijn twee dwarsdoorsneden (D1 en D2) voorgedefinieerd, wat resulteert in één brugsegment.
 Pas de waarden aan, of voeg meer dwarsdoorsneden toe/verwijder ze via de '+' en '-' knoppen."""
     )
@@ -61,7 +62,7 @@ Pas de waarden aan, of voeg meer dwarsdoorsneden toe/verwijder ze via de '+' en 
                 "bz2": 5.0,
                 "bz3": 15.0,
                 "dz": 2.0,
-                "dze": 1.0,
+                "dz_2": 3.0,
                 "col_6": 0.0,
                 "l": 0,
                 "is_first_segment": True,
@@ -71,7 +72,7 @@ Pas de waarden aan, of voeg meer dwarsdoorsneden toe/verwijder ze via de '+' en 
                 "bz2": 5.0,
                 "bz3": 15.0,
                 "dz": 2.0,
-                "dze": 1.0,
+                "dz_2": 3.0,
                 "col_6": 0.0,
                 "l": 10,
                 "is_first_segment": False,
@@ -84,7 +85,7 @@ Pas de waarden aan, of voeg meer dwarsdoorsneden toe/verwijder ze via de '+' en 
     input.dimensions.array.bz2 = NumberField("Breedte zone 2", default=5.0, suffix="m")
     input.dimensions.array.bz3 = NumberField("Breedte zone 3", default=15.0, suffix="m")
     input.dimensions.array.dz = NumberField("Dikte zone 1 en 3", default=2.0, suffix="m")
-    input.dimensions.array.dze = NumberField("Extra dikte zone 2", default=1.0, suffix="m")
+    input.dimensions.array.dz_2 = NumberField("Dikte zone 2", default=3.0, suffix="m")
     input.dimensions.array.col_6 = NumberField("alpha", default=0.0, suffix="Graden")
 
     _l_field_visibility_constraint = DynamicArrayConstraint(
